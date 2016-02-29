4462d746fbeef53b241d83ce1666bd9575b8c4ed
tmharkins <tmharkins@gmail.com>
2016-02-28 19:02:49 -0600

    parsed CSV and 'Space SV' emails at the same time

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,18 @@
 # Build a class EmailParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
-# unique email addresses. The delimiters to support are commas (',')
+# unique email addresses. The delimiters to support are commas (',') 
+class EmailParser
+
+	attr_accessor :email
+
+	def initialize(email)
+		@email = email
+	end
+
+	def parse
+		no_comma_email = email.gsub(/,/, " ")
+		parse = no_comma_email.split(" ")
+		no_dups_parse = parse.uniq
+	end
+
+end
